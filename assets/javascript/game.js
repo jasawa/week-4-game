b40636c0c70ca8c96b316af7ada655f7f5a9b7ba
Jane Asawa <asawajane@gmail.com>
2018-03-21 00:54:45 -0700

    output is working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,16 +11,20 @@ var crystalNum = [0, 0, 0, 0];
 
 
 function startNewGame() {
+        // reset sum to 0 and display 0 for user
         sum = 0;
+        $("#sum").text(sum);
         // Computer assigns a random number between 19 and 120 inclusive that the user will try to match
         matchNum = Math.floor(Math.random() * 102) + 19;
         console.log(matchNum);
+        $("#number-to-match").text(matchNum);
 
         // Computer assigns different random numbers between 1 and 12 inclusive to each of the crystals 
         for (var i=0; i<4; i++) {
             crystalNum[i] = Math.floor(Math.random() * 12) + 1;
         }
-        console.log(crystalNum); 
+        console.log(crystalNum);
+
 }
 
 
@@ -28,38 +32,49 @@ function playGame() {
     if (sum === matchNum) {
             wins = wins + 1;
             console.log("wins are " + wins);
+            $("#win-or-lose").text("You win!");
+            $("#Wins").html("Wins: " + wins);
             startNewGame();
     }
     if (sum > matchNum) {
             losses = losses + 1;
             console.log("losses are " + losses);
+            $("#win-or-lose").text("Sorry, you lose.");
+            $("#Losses").html("Losses: " + losses);
             startNewGame();
-    }  
+    } 
 } 
-        // a cystal is clicked and add its value to the sum
+
+
+        // a crystal is clicked and add its value to the sum
         $("#amethyst").on("click", function () {
             sum = sum + crystalNum[0];
             console.log("amethyst " + crystalNum[0] + " Sum is " + sum);
+            $("#sum").text(sum);
             playGame();
         });
 
         $("#blue").on("click", function () {
             sum = sum + crystalNum[1];
             console.log("blue " + crystalNum[1] + " Sum is " + sum);
+            $("#sum").text(sum);
             playGame();
         });
 
         $("#quartz").on("click", function () {
             sum = sum + crystalNum[2];
             console.log("quartz " + crystalNum[2] + " Sum is " + sum);
+            $("#sum").text(sum);
             playGame();
         });
 
         $("#ruby").on("click", function () {
             sum = sum + crystalNum[3];
             console.log("ruby " + crystalNum[3] + " Sum is " + sum);
+            $("#sum").text(sum);
             playGame();
         });
+
 
 playGame();
 startNewGame();
